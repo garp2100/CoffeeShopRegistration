4fc48530f755f31549b5da414f21281e03ba6fd8
Gustavo Rivera <garp2100@gmail.com>
2022-09-15 21:50:20 -0400

    Add project files.

---
+++ CoffeeShopRegistration/Models/CoffeeShopRegistrationContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeShopRegistration.Models
+{
+    public partial class CoffeeShopRegistrationContext : DbContext
+    {
+        public CoffeeShopRegistrationContext()
+        {
+        }
+
+        public CoffeeShopRegistrationContext(DbContextOptions<CoffeeShopRegistrationContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=CoffeeShopRegistration; Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Email).HasMaxLength(20);
+
+                entity.Property(e => e.FirstName).HasMaxLength(20);
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.LastName).HasMaxLength(20);
+
+                entity.Property(e => e.Password).HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
